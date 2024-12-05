f79571f9403338a8f93aa69cda4b9af31c860de4
Matheussmaced <mateusbyakuya3@gmail.com>
2024-12-05 10:44:48 -0300

    feat: creating header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {path: 'login', component: LoginPageComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { InputsLoginComponent } from './components/inputs-login/inputs-login.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Bell, ChevronDown, CircleUserRound, LucideAngularModule, Menu } from 'lucide-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     InputsLoginComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    LucideAngularModule.pick({ Menu, Bell, CircleUserRound, ChevronDown })
   ],
   providers: [],
   bootstrap: [AppComponent]
