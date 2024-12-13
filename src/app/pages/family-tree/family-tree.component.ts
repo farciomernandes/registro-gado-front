e1ede781d12f19901dc79bc82a293b2235bd0bda
Matheussmaced <mateusbyakuya3@gmail.com>
2024-12-13 17:04:47 -0300

    fix: id for family tree component

--- src/app/pages/family-tree/family-tree.component.ts
+++ src/app/pages/family-tree/family-tree.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-family-tree',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './family-tree.component.scss'
 })
 export class FamilyTreeComponent {
+  id: string | null = null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.id = params['id']; // Captura o id
+      console.log('Animal ID recebido:', this.id);
+    });
+  }
 }
