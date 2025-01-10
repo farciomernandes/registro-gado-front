96c70470ad84f8dd0a67e3438ddc9968ccfa6471
Matheussmaced <mateusbyakuya3@gmail.com>
2025-01-10 12:02:35 -0300

    feat: search with purity

--- src/app/components/home/home-list/home-list.component.ts
+++ src/app/components/home/home-list/home-list.component.ts
@@ -1,18 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NewAnimalModalComponent } from '../../modal/new-animal-modal/new-animal-modal.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AnimalService } from '../../../services/animal.service';
 
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
   styleUrl: './home-list.component.scss'
 })
-export class HomeListComponent {
-  constructor(public dialog: MatDialog) {}
+export class HomeListComponent implements OnInit{
+  constructor(public dialog: MatDialog, private animalService: AnimalService) {}
 
   selectedSex: string = '';
+  purities: string[] = []
+  selectedPurity: string = '';
 
   searchCriteria: { field: string; value: string } | null = null;
+
+  ngOnInit(): void {
+    this.fetchPurities();
+  }
+
+  fetchPurities(): void {
+    this.animalService.getAnimals().subscribe(
+      (animals) => {
+        this.purities = [...new Set(animals.map((animal) => animal.purity))].filter(Boolean);
+        console.log('Purezas disponíveis:', this.purities);
+      },
+      (error) => {
+        console.error('Erro ao buscar purezas:', error);
+      }
+    );
+  }
+
+  onPurityChange(): void {
+    if (!this.selectedPurity) {
+      this.searchCriteria = null;
+    } else {
+      this.searchCriteria = { field: 'purity', value: this.selectedPurity };
+    }
+    console.log('Critério de pureza atualizado:', this.searchCriteria);
+  }
 
   onSearchCriteriaChange(criteria: { field: string; value: string }) {
     this.searchCriteria = criteria;
