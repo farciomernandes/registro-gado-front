ba9595f5e82696dfea9e3daa5e7355f3c90b240b
Marcio Fernandes <farciomernandes@gmail.com>
2025-04-08 15:23:41 -0300

    credentials fb

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,19 +1,18 @@
 export const environment = {
   product: false,
   apiUrl: "https://registro-de-gado-backend.onrender.com",
-  //apiUrl: "http://localhost:8080",
 }
 import { initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "-..",
+  apiKey: "AIzaSyBkEAZuC3iYnDeZFG21DmlxXNX6iI_2j58",
+  authDomain: "registro-gado.firebaseapp.com",
   projectId: "registro-gado",
-  storageBucket: "-gado..app",
-  messagingSenderId: "",
-  appId: "1:::",
-  measurementId: "G-",
-  vapidKey: "-",
+  storageBucket: "registro-gado.firebasestorage.app",
+  messagingSenderId: "1030379429431",
+  appId: "1:1030379429431:web:f8681207a5d71c3ba929b2",
+  measurementId: "G-RLHRCMLWEE",
+  vapidKey: "BFkKgTMDjYfNOQV_QpdA1tdkAb7JLxvJXawo3wTHF2YxwXtwNEofWb0wQLW4xohB9715vp3Jde-FextXLmlp_Ys",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,15 +1,15 @@
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "",
-  authDomain: "-gado..com",
-  projectId: "-gado",
-  storageBucket: "-gado..app",
-  messagingSenderId: "",
-  appId: "1::web:",
-  measurementId: "G-",
-  vapidKey: "=="
+firebase.initializeApp( {
+  apiKey: "AIzaSyBkEAZuC3iYnDeZFG21DmlxXNX6iI_2j58",
+  authDomain: "registro-gado.firebaseapp.com",
+  projectId: "registro-gado",
+  storageBucket: "registro-gado.firebasestorage.app",
+  messagingSenderId: "1030379429431",
+  appId: "1:1030379429431:web:f8681207a5d71c3ba929b2",
+  measurementId: "G-RLHRCMLWEE",
+  vapidKey: "BFkKgTMDjYfNOQV_QpdA1tdkAb7JLxvJXawo3wTHF2YxwXtwNEofWb0wQLW4xohB9715vp3Jde-FextXLmlp_Ys",
 });
 
 const messaging = firebase.messaging();
